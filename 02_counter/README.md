668bd122536665f20d73418e0c27ca24cb15d6f5
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-06-01 11:34:25 +0530

    Hooks and First Project

---
+++ 02_counter/README.md
@@ -0,0 +1,15 @@
+# React Project and Hooks
+
+## Learn
+
+    1- I learn how to how to update values in Ui and change state of Ui.
+    2- I understand the how react Hook (useState()) works.
+    3- I learn how react do updation on ui automaticaly using useState hooks in whole page.
+
+## how to import useState() hook
+
+    import { useState } from "react";
+
+## how to use useState() hook
+
+    let [counter, setCounter] = useState(1); // use any name in the place of counter, setCounter
---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"; // import useState hook from react
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  // react don't allow you  to updation on ui by simple js code , it's done by it's own method called hooks
+
+  let [counter, setCounter] = useState(1); // you can use any name in place of counter and setCounter
+
+  // let counter = 1;
+
+  const addValue = () => {
+    // counter = counter + 1;
+    if (counter < 20) {
+      setCounter(counter + 1); // only update ui state not value of variable
+      console.log("Value Added", counter + 1);
+    }
+  };
+
+  const removeValue = () => {
+    // counter = counter - 1;
+    if (counter > 0) {
+      setCounter(counter - 1);
+      console.log("Value Removed", counter - 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>React Project</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  );
+}
+
+export default App;
